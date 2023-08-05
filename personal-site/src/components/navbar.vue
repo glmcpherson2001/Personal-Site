369c84fdd4fc7faefd6f9541767c39d55b58dba6
glmcpherson2001 <glmcpherson2001@gmail.com>
2023-08-05 12:20:18 -0500

    Setup Tailwind CSS & laid foundation for Navbar

--- personal-site/src/components/navbar.vue
+++ personal-site/src/components/navbar.vue
@@ -1,20 +1,42 @@
 <template>
-        
-  </template>
-  
-  <script>
-  // Your JavaScript logic goes here
-  export default {
-    name: 'Top-Navbar',
-    data() {
-      return {
-      };
-    }
-    // Other component options like methods, computed properties, etc., can go here
-  }
-  </script>
-  
-  <style scoped>
-    
-  </style>
-  
+  <header class="sticky top-0 bg-white">
+    <nav>
+      <ul class="flex align-middle justify-around p-5" :class="{'bg-blue-500': scrolledDown}">
+        <li v-for="(item, index) in navLinks" :key="index"><router-link to="/">{{ item.name }}</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+import router from './../router'
+
+export default {
+  name: 'Top-Navbar',
+  data() {
+    return {
+      scrolledDown: false,
+      navLinks: router.options.routes
+    };
+  },
+  methods: {
+    handleScroll() {
+      // Check the scroll position and update the 'scrolledDown' data property
+      this.scrolledDown = window.scrollY > 0;
+    },
+  },
+  mounted() {
+    // Add a scroll event listener to the window
+    console.log(this.navLinks)
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeUnmount() {
+    // Don't forget to remove the event listener when the component is unmounted
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- personal-site/src/router.js
+++ personal-site/src/router.js
@@ -1,36 +1,39 @@
-import { createRouter} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-import index from './components/index.vue'
-import contact from './components/contact.vue'
-import about from './components/about.vue'
-import portfolio from './components/portfolio.vue'
-
+import index from './views/index.vue'
+import contact from './views/contact.vue'
+import about from './views/about.vue'
+import portfolio from './views/portfolio.vue'
 
 
 
 
 const routes = [
     {
+        name: 'Grant L. McPherson',
         path: '/',
         component: index
     },
     {
+        name: 'Portfolio',
         path: '/portfolio',
         component:  portfolio
     },
     {
+        name: 'About',
         path: '/about',
         component:  about
     },
     {
+        name: 'Contact Me!',
         path: '/contact',
         component:  contact
-    },
-    
-
+    }
 ]
+const history = createWebHistory();
 
 const router = createRouter({
+    history,
     routes
 })
 
